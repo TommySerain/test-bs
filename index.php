dded6a7b02b5b125e0bbeed6f34a076f11edb5bd
TommySerain <tommyserain@yahoo.fr>
2023-05-30 22:00:02 +0200

    modif de l'index pour test

--- index.php
+++ index.php
@@ -10,40 +10,37 @@ use App\Entity\Tarif;
 
 try {
 
-    // $clients= new Client();
-    // $client=$clients->getClientById(6);
+    $clients = new Client();
+    $client = $clients->getClientById(6);
     // $city=$client['ville'];
     // // $client=$clients->getClientByName("VINS DU MACONNAIS");
 
-    // echo "<pre>";
-    // print_r($client);
-    // echo "</pre>";
+    echo "<pre>";
+    print_r($client);
+    echo "</pre>";
 
-    // $localites= new Localite();
-    // $localite=$localites->getLocaliteByZipCode("69");
-    // $localite=$localites->getLocaliteByCity("AMPUIS");
+    $localites = new Localite();
+    // $localite=$localites->getLocaliteByZipCode("71");
+    $localite = $localites->getLocaliteByCity("MACON");
     // $localite=$localites->getLocaliteByZone(2);
 
-    // echo "<pre>";
-    // print_r($localite);
-    // echo "</pre>";
+    echo "<pre>";
+    print_r($localite);
+    echo "</pre>";
 
-    // $conditions= new ConditionTaxation();
-    // $condition=$conditions->getConditionById(1);
-    // $localite=$localites->getLocaliteByCity("AMPUIS");
-    // $localite=$localites->getLocaliteByZone(2);
+    $conditions = new ConditionTaxation();
+    $condition = $conditions->getConditionById(1);
 
-    // echo "<pre>";
-    // print_r($condition);
-    // echo "</pre>";
+    echo "<pre>";
+    print_r($condition);
+    echo "</pre>";
 
-    $tarifs= new Tarif();
-    $tarif=$tarifs->getTarifByCodeDepartement(70);
+    $tarifs = new Tarif();
+    $tarif = $tarifs->getTarifByCodeDepartement(71);
 
     echo "<pre>";
     print_r($tarif);
     echo "</pre>";
-
-}catch(Exception $e){
+} catch (Exception $e) {
     echo $e->getMessage();
 };
