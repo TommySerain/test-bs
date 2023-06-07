dd84cdd35a110ce126ba385190f6fc2f2f7482c1
TommySerain <tommyserain@yahoo.fr>
2023-06-07 13:23:50 +0200

    légère modif sur le visuel

--- src/Template/form.php
+++ src/Template/form.php
@@ -1,7 +1,7 @@
 <form class="row mt-5" action="" method="POST">
         <div class="col-6 mx-auto mt-4">
             <label for="sender">Expéditeur</label>
-            <select name="sender" id="sender" class="form-control border-2 border-danger rounded-2 bg-success text-white" required>
+            <select name="sender" id="sender" class="form-control border-3 border-danger rounded-2 bg-success text-white" required>
                 <?php
                 foreach ($allClients as $client) { ?>
                     <option value="<?php echo $client['idClient'] ?>"><?php echo $client['raisonSociale'] ?></option>
@@ -12,7 +12,7 @@
         </div>
         <div class="col-6 mx-auto mt-4">
             <label for="recipient">Destinataire</label>
-            <select name="recipient" id="recipient" class="form-control border-2 border-danger rounded-2 bg-success text-white" required>
+            <select name="recipient" id="recipient" class="form-control border-3 border-danger rounded-2 bg-success text-white" required>
                 <?php
                 foreach ($allClients as $client) { ?>
                     <option value="<?php echo $client['idClient'] ?>"><?php echo $client['raisonSociale'] ?></option>
@@ -23,11 +23,11 @@
         </div>
         <div class="col-4 mt-4">
             <label for="number">Nombre de colis</label>
-            <input class="form-control border-2 border-danger rounded-2 bg-success text-white" type="number" name="number" id="number" required>
+            <input class="form-control border-3 border-danger rounded-2 bg-success text-white" type="number" name="number" id="number" required>
         </div>
         <div class="col-4 mt-4">
             <label for="weight">Poids de l'expédition</label>
-            <input class="form-control border-2 border-danger rounded-2 bg-success text-white" type="number" name="weight" id="weight" required>
+            <input class="form-control border-3 border-danger rounded-2 bg-success text-white" type="number" name="weight" id="weight" required>
         </div>
         <div class="col-4 mt-auto">
             <div>
@@ -39,5 +39,5 @@
                 <input type="radio" name="taxes" id="taxes-recipient" value="2" required>
             </div>
         </div>
-        <input class="btn btn-success mt-4 col-3 mx-auto border-2 border-danger" type="submit" value="Calculer">
+        <input class="btn btn-success mt-4 col-3 mx-auto border-3 border-danger" type="submit" value="Calculer">
     </form>
